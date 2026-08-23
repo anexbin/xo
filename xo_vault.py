8e6374a9d8b655e33b98b4483365b35693b2a183
AnesX <your-email@example.com>
2026-08-23 15:54:11 +0100

    figuring the first script out

---
+++ xo_vault.py
@@ -0,0 +1,51 @@
+
+xo_vault = str(input("do you wanna start working with the xo vault y/n ?").lower())
+if not xo_vault == y or xo_vault == xo_vault :
+    print("Bro its even a yes or no y/n ")
+else:
+    while xo_vault == "y" :
+        user_action = str(input("What do you wanna do :
+                        1. add a song 
+                        2. See all the songs 
+                        3. Filter by album 
+                        "))
+         if user_action == 1:
+             the_album = str(input(
+             the_song = str(input("Enter the song :"))
+             the_rating = str(input("Enter you rating of the song :"))
+             the_url = str(input("Enter the URL :"))
+
+
+
+
+        
+
+
+
+
+
+                     albums = {
+                         "House of Balloons (2011)":{}
+                         "Thursday (2011)":{}
+                         "Echoes of Silence (2011)":{}
+                         "Kiss Land (2013)":{}
+                         "After Hours (2020)":{}
+                         "Dawn FM (2022)":{}
+                         "Hurry Up Tomorrow (2025)":{}
+                         "Collabs":{}
+                         "Unreleased":{}
+
+
+
+                         }
+
+
+       
+
+
+
+
+
+
+
+
