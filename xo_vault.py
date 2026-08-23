ac837e885b64de41c3d11a921bd699950d7eafb4
AnesX <your-email@example.com>
2026-08-23 23:30:46 +0100

    making stuff more organized

--- xo_vault.py
+++ xo_vault.py
@@ -1,8 +1,23 @@
-
+ # the albums
+album = {"House of Balloons (2011)":{}
+         "Thursday (2011)":{}
+         "Echoes of Silence (2011)":{}
+         "Kiss Land (2013)"{}
+         "Beauty Behind the Madness (2015)"{}
+         "Starboy (2016)"{}
+         "After Hours (2020)":{}
+         "Dawn FM (2022)":{}
+         "Hurry Up Tomorrow (2025)":{}
+         "Collabs":{}
+         "Unreleased":{}
+          }
+                         
+                         
 xo_vault = str(input("do you wanna start working with the xo vault y/n ?").lower())
 if not xo_vault == y or xo_vault == xo_vault :
     print("Bro its even a yes or no y/n ")
 else:
+
     while xo_vault == "y" :
         user_action = str(input("What do you wanna do :
                         1. add a song 
@@ -10,11 +25,15 @@ else:
                         3. Filter by album 
                         "))
          if user_action == 1:
-             the_album = str(input(
+             the_album = str(input("Enter the album "))
              the_song = str(input("Enter the song :"))
              the_rating = str(input("Enter you rating of the song :"))
              the_url = str(input("Enter the URL :"))
+             the_song = {"{the_rating}, {the_url}"} 
+            
+            
 
+                                 
 
 
 
@@ -23,17 +42,6 @@ else:
 
 
 
-
-                     albums = {
-                         "House of Balloons (2011)":{}
-                         "Thursday (2011)":{}
-                         "Echoes of Silence (2011)":{}
-                         "Kiss Land (2013)":{}
-                         "After Hours (2020)":{}
-                         "Dawn FM (2022)":{}
-                         "Hurry Up Tomorrow (2025)":{}
-                         "Collabs":{}
-                         "Unreleased":{}
 
 
 
