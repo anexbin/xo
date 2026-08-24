a94e0682ad560a06764f30399fa4fbef30d2ea14
AnesX <your-email@example.com>
2026-08-24 12:27:20 +0100

    errors handling and the V1 of the program

--- xo_vault.py
+++ xo_vault.py
@@ -1,45 +1,62 @@
  # the albums
-albums = {"House of Balloons (2011)":{}
-         "Thursday (2011)":{}
-         "Echoes of Silence (2011)":{}
-         "Kiss Land (2013)"{}
-         "Beauty Behind the Madness (2015)"{}
-         "Starboy (2016)"{}
-         "After Hours (2020)":{}
-         "Dawn FM (2022)":{}
-         "Hurry Up Tomorrow (2025)":{}
-         "Collabs":{}
-         "Unreleased":{}
+albums = {
+        "House of Balloons (2011)":{},
+        "Thursday (2011)":{},
+        "Echoes of Silence (2011)":{},
+        "Kiss Land (2013)":{},
+        "Beauty Behind the Madness (2015)":{},
+        "Starboy (2016)":{},
+        "After Hours (2020)":{},
+        "Dawn FM (2022)":{},
+        "Hurry Up Tomorrow (2025)":{},
+        "Collabs":{},
+        "Unreleased":{},
           }
                          
                          
-xo_vault = str(input("do you wanna start working with the xo vault y/n ?").lower())
-if not xo_vault == y or xo_vault == xo_vault :
-    print("Bro its even a yes or no y/n ")
-else:
+xo_vault = str(input("do you wanna start working with the xo vault y/n ?:").lower())
 
+if xo_vault == "n":
+    print("See you Bro")
+elif xo_vault == "y" :
     while xo_vault == "y" :
-        user_action = str(input("What do you wanna do :
-                        1. add a song 
-                        2. See all the songs 
-                        3. Filter by album 
-                        "))
-         if user_action == 1:
-             the_album = str(input("Enter the album name L "))
-             the_song = str(input("Enter the song name  :"))
-             the_rating = str(input("Enter you rating of the song name :"))
-             the_url = str(input("Enter the URL :"))
-             the_song_details = {"{the_rating}, {the_url}"} 
+        user_action = int(input("""
+What do you wanna do :
+1. add a song 
+2. See all the songs 
+3. Filter by album
+4. Exit
+>:"""))
+
+        if user_action == 1:
+            the_album = str(input("Enter the album name : "))
+            the_song = str(input("Enter the song name  :"))
+            the_rating = str(input("Enter you rating of the song name :"))
+            the_url = str(input("Enter the URL :"))
+            the_song_details = {"{the_rating}, {the_url}"} 
              
-            albums[the_album][the_song].append(the_song_details)
+            albums[the_album].setdefault(the_song, []).append(the_song_details)
         elif user_action == 2:
             print(albums)
         elif user_action == 3:
             the_album = str(input("Enter the album name "))
             for key in albums[the_album]:
                 print(key)
+        elif user_action == 4:
+            xo_vault = "n"
+
+            print("See you Bro")
         else:
-            print("Bro choose 1/2/3 there is nothing much to do right now ")
+            print("You choose what to do with the typing the first number in each argument")
+else:
+    print("Bro its even y or n ")
+
+            
+            
+
+    
+
+
 
 
 
@@ -58,7 +75,7 @@ else:
 
 
 
-                         }
+                         
 
 
        
