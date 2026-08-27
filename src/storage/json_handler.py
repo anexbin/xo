19df8514a940c15633948390b5caa9e8209d9ebe
AnesX <your-email@example.com>
2026-08-27 22:32:05 +0100

    pretty tired just modified on the track_handler.py

---
+++ src/storage/json_handler.py
@@ -0,0 +1,12 @@
+import json
+import os
+from dataclasses import asdict
+from ..models.track import Track
+
+def load_tracks():
+    if not os.path.exist("I will fill it later with the actual path "):
+        return []
+    try:
+        with open("the name or path ", "r") as f:
+
+            
